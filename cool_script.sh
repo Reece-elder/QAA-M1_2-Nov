601c8584ce1bba258b2a69352c89ebe7dbb7247d
reece-elder <reece.elder@qa.com>
2022-11-23 13:42:04 +0000

    changed workflow and added script

---
+++ cool_script.sh
@@ -0,0 +1,10 @@
+# A script is a piece of code that contains shell commands 
+# that operate the same as writing in terminal
+
+# shebang which tells this script how we want it to run
+#! /bin/bash
+
+echo "Hello world"
+pwd
+ls -a
+cat README.md
